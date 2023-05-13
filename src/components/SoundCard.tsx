3afd0b9503e3af885100ac26f6ca224721bd6771
Cameron Lund <cameronlund4@gmail.com>
2023-05-13 14:37:30 -0400

    Add some favorited state

--- src/components/SoundCard.tsx
+++ src/components/SoundCard.tsx
@@ -1,4 +1,5 @@
 import { DocumentDuplicateIcon, EnvelopeIcon, HeartIcon, PhoneIcon, PlayIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 import { username, loremIpsum } from 'react-lorem-ipsum';
 
 type Props = {
@@ -15,6 +16,9 @@ const PlayButton: React.FC<Props> = ({ }) => {
 
 const SoundCard: React.FC<Props> = ({ }) => {
     const playCount = Math.floor(Math.random() * 4) + 1;
+    const [favorited, setFavorited] = useState(Math.random() > 0.5);
+    const notFavoritedTheme = "text-gray-400 hover:cursor-pointer hover:text-pink-400";
+    const favoritedTheme = "text-pink-400 hover:cursor-pointer hover:text-gray-400";
 
     return <li
         className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white shadow"
@@ -40,10 +44,10 @@ const SoundCard: React.FC<Props> = ({ }) => {
                 </div>
                 <div className="-ml-px flex w-0 flex-1">
                     <a
-                        className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-400 hover:cursor-pointer hover:text-pink-400"
+                        className={`relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold ${favorited ? favoritedTheme : notFavoritedTheme}`}
                     >
                         <HeartIcon className="h-5 w-5 " aria-hidden="true" />
-                        Save
+                        {favorited ? "Favorited" : "Favorite"}
                     </a>
                 </div>
             </div>
