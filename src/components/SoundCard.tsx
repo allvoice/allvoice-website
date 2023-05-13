960e46fd778b0cde56f3dbaec51b0e0157f5cb16
Cameron Lund <cameronlund4@gmail.com>
2023-05-13 14:31:30 -0400

    Start working on sound cards

---
+++ src/components/SoundCard.tsx
@@ -0,0 +1,54 @@
+import { DocumentDuplicateIcon, EnvelopeIcon, HeartIcon, PhoneIcon, PlayIcon } from "@heroicons/react/20/solid";
+import { username, loremIpsum } from 'react-lorem-ipsum';
+
+type Props = {
+};
+
+const PlayButton: React.FC<Props> = ({ }) => {
+    const faces = ["😭", "🤬", "😁"]
+    const face = faces[Math.floor(Math.random() * faces.length)]
+    return <div className="p-2 rounded-full border-gray-400 border border-solid shadow text-gray-400 hover:cursor-pointer hover:text-blue-400 hover:border-blue-400">
+        {/* <PlayIcon className="h-5 w-5" aria-hidden="true" /> */}
+        {face}
+    </div>
+}
+
+const SoundCard: React.FC<Props> = ({ }) => {
+    const playCount = Math.floor(Math.random() * 4) + 1;
+
+    return <li
+        className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white shadow"
+    >
+        <div className="flex flex-1 flex-col p-8">
+            <h1 className="text-xl">{username()}</h1>
+            <div className="flex space-x-3 mt-2">
+                {Array.from({ length: playCount }, () => <PlayButton />)}
+            </div>
+            <div>
+                {loremIpsum({ p: 1, avgSentencesPerParagraph: 3 })}
+            </div>
+        </div>
+        <div>
+            <div className="-mt-px flex divide-x divide-gray-200">
+                <div className="flex w-0 flex-1">
+                    <a
+                        className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-400  hover:cursor-pointer hover:text-blue-400"
+                    >
+                        <DocumentDuplicateIcon className="h-5 w-5" aria-hidden="true" />
+                        Clone
+                    </a>
+                </div>
+                <div className="-ml-px flex w-0 flex-1">
+                    <a
+                        className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-400 hover:cursor-pointer hover:text-pink-400"
+                    >
+                        <HeartIcon className="h-5 w-5 " aria-hidden="true" />
+                        Save
+                    </a>
+                </div>
+            </div>
+        </div>
+    </li>;
+}
+
+export default SoundCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { type NextPage } from "next";
+import SoundCard from "~/components/SoundCard";
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen bg-red-500">
-      <p>Hello! Testing the layout.</p>
-    </div>
+    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <SoundCard />
+      <SoundCard />
+      <SoundCard />
+      <SoundCard />
+      <SoundCard />
+    </ul>
   );
 };
 
