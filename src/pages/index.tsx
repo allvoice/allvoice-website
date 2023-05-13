3b3d39168a82430b4b3eeb69823799532c812b83
Cameron Lund <cameronlund4@gmail.com>
2023-05-13 13:38:09 -0400

    Set up layout, style enforcement, self-hosted runners (#1)
    Co-authored-by: Evan Pierce <mrthinger@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
 import { type NextPage } from "next";
 
-import Header from "~/components/header";
-
 const Home: NextPage = () => {
-
   return (
-    <div className="min-h-full">
-    <Header />
-
-    <main className="h-[calc(100vh-theme('spacing.16'))]">
-      <div className="mx-auto flex max-w-7xl flex-col space-y-4 px-2 py-2 sm:px-6 md:py-10 lg:px-8">
-     
-      </div>
-    </main>
-  </div>
+    <div className="h-screen bg-red-500">
+      <p>Hello! Testing the layout.</p>
+    </div>
   );
 };
 
