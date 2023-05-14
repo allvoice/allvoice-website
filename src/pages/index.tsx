fabe956b2e03ba709b5849687fd4f5ee8f5bb71d
Evan Pierce <mrthinger@gmail.com>
2023-05-14 02:14:35 -0700

    basic s3 test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { type NextPage } from "next";
-
+import { api } from "~/utils/api";
 const Home: NextPage = () => {
+
+  const exp = api.example.hello.useQuery({text: "test"})
+
   return (
     <div className="h-screen bg-red-500">
       <p>Hello! Testing the layout.</p>
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -2,10 +2,13 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+import { testUploadGetDelete } from "~/server/s3";
+
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query(async ({ input }) => {
+      await testUploadGetDelete();
       return {
         greeting: `Hello ${input.text}`,
       };
