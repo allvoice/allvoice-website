d3545b03933bd4cfcc6e3dab682dc8f79349ad1e
Evan Pierce <mrthinger@gmail.com>
2023-05-14 18:12:24 -0700

    fix lint & style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
 const Home: NextPage = () => {
-
-  const exp = api.example.hello.useQuery({text: "test"})
+  api.example.hello.useQuery({ text: "test" });
 
   return (
     <div className="h-screen bg-red-500">
