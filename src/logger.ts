10593ac6bd5b5e2fab864d64b3dc4460fd214b2a
Evan Pierce <mrthinger@gmail.com>
2023-12-03 15:08:37 -0800

    add winston logger

---
+++ src/logger.ts
@@ -0,0 +1,24 @@
+import winston from 'winston';
+import { env } from './env.mjs';
+
+
+const logger = winston.createLogger({
+  level: env.LOG_LEVEL,
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+export default logger;
