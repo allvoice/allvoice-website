0fc40a3ab0de82196ae1929d1f203b4a9108d8c3
Evan Pierce <mrthinger@gmail.com>
2023-12-03 14:07:07 -0800

    db reset helper

---
+++ scripts/reset-helper.mjs
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function deleteVoicesAndSeedSounds() {
+    // Set all forkParentId fields to null (doesnt work i think due to ciclical relation)
+    // await prisma.voiceModel.updateMany({
+    //   data: {
+    //     forkParentId: null,
+    //   },
+    // });
+  // Delete related records first
+  await prisma.voiceModelSeedSounds.deleteMany();
+  await prisma.seedSound.deleteMany();
+  await prisma.previewSound.deleteMany();
+  await prisma.voiceModel.deleteMany();
+  await prisma.favorite.deleteMany();
+  await prisma.vote.deleteMany();
+
+  // Then delete the main records
+  await prisma.voice.deleteMany();
+}
+
+deleteVoicesAndSeedSounds()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  });
