87f4ac3570d9a1972a62b3abb01a1ad7d5bc7c4f
Evan Pierce <mrthinger@gmail.com>
2023-12-03 17:19:17 -0800

    separate fork from non forked flow

--- src/pages/voicemodels/create.tsx
+++ src/pages/voicemodels/create.tsx
@@ -7,6 +7,89 @@ type Props = {
   statusCode?: number;
 };
 
+const createForkedVoice = async (forkVoiceId: string, userId: string) => {
+  const voiceToBeForked = await prisma.voice.findUniqueOrThrow({
+    where: {
+      id: forkVoiceId,
+    },
+  });
+
+  const newVoice = await prisma.voice.create({
+    data: {
+      ownerUser: {
+        connectOrCreate: { where: { id: userId }, create: { id: userId } },
+      },
+      forkParent: { connect: { id: voiceToBeForked.id } },
+      ...(voiceToBeForked.uniqueWarcraftNpcId
+        ? {
+            uniqueWarcraftNpc: {
+              connect: { id: voiceToBeForked.uniqueWarcraftNpcId },
+            },
+          }
+        : {}),
+      ...(voiceToBeForked.warcraftNpcDisplayId
+        ? {
+            warcraftNpcDisplay: {
+              connect: { id: voiceToBeForked.warcraftNpcDisplayId },
+            },
+          }
+        : {}),
+    },
+  });
+
+  const voiceModels = await prisma.voiceModel.findMany({
+    where: {
+      voiceId: forkVoiceId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 1,
+    include: {
+      soundFileJoins: true,
+    },
+  });
+
+  const voiceModel = voiceModels[0];
+  if (!voiceModel) {
+    return {
+      props: {
+        statusCode: 500,
+      },
+    };
+  }
+
+  const newVoiceModel = await prisma.voiceModel.create({
+    data: {
+      voice: { connect: { id: newVoice.id } },
+      forkParent: { connect: { id: voiceModel.id } },
+      elevenLabsModelId: voiceModel.elevenLabsModelId,
+      elevenLabsStability: voiceModel.elevenLabsStability,
+      elevenLabsSimilarityBoost: voiceModel.elevenLabsSimilarityBoost,
+      elevenLabsStyle: voiceModel.elevenLabsStyle,
+      elevenLabsSpeakerBoost: voiceModel.elevenLabsSpeakerBoost,
+      published: false,
+    },
+  });
+
+  for (const seedSoundJoin of voiceModel.soundFileJoins) {
+    await prisma.voiceModelSeedSounds.create({
+      data: {
+        voiceModel: { connect: { id: newVoiceModel.id } },
+        seedSound: { connect: { id: seedSoundJoin.seedSoundId } },
+        active: seedSoundJoin.active,
+      },
+    });
+  }
+
+  return {
+    redirect: {
+      destination: `/voicemodels/${newVoiceModel.id}/edit`,
+      permanent: false,
+    },
+  };
+};
+
 export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
   const { userId } = getAuth(ctx.req);
   if (!userId) {
@@ -22,86 +105,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
   const forkVoiceId = ctx.query.fork as string | undefined;
 
   if (forkVoiceId) {
-    const voiceToBeForked = await prisma.voice.findUniqueOrThrow({
-      where: {
-        id: forkVoiceId,
-      },
-    });
-
-    const newVoice = await prisma.voice.create({
-      data: {
-        ownerUser: {
-          connectOrCreate: { where: { id: userId }, create: { id: userId } },
-        },
-        forkParent: { connect: { id: voiceToBeForked.id } },
-        ...(voiceToBeForked.uniqueWarcraftNpcId
-          ? {
-              uniqueWarcraftNpc: {
-                connect: { id: voiceToBeForked.uniqueWarcraftNpcId },
-              },
-            }
-          : {}),
-        ...(voiceToBeForked.warcraftNpcDisplayId
-          ? {
-              warcraftNpcDisplay: {
-                connect: { id: voiceToBeForked.warcraftNpcDisplayId },
-              },
-            }
-          : {}),
-      },
-    });
-
-    const voiceModels = await prisma.voiceModel.findMany({
-      where: {
-        voiceId: forkVoiceId,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-      take: 1,
-      include: {
-        soundFileJoins: true,
-      },
-    });
-
-    const voiceModel = voiceModels[0];
-    if (!voiceModel) {
-      return {
-        props: {
-          statusCode: 500,
-        },
-      };
-    }
-
-    const newVoiceModel = await prisma.voiceModel.create({
-      data: {
-        voice: { connect: { id: newVoice.id } },
-        forkParent: { connect: { id: voiceModel.id } },
-        elevenLabsModelId: voiceModel.elevenLabsModelId,
-        elevenLabsStability: voiceModel.elevenLabsStability,
-        elevenLabsSimilarityBoost: voiceModel.elevenLabsSimilarityBoost,
-        elevenLabsStyle: voiceModel.elevenLabsStyle,
-        elevenLabsSpeakerBoost: voiceModel.elevenLabsSpeakerBoost,
-        published: false,
-      },
-    });
-
-    for (const seedSoundJoin of voiceModel.soundFileJoins) {
-      await prisma.voiceModelSeedSounds.create({
-        data: {
-          voiceModel: { connect: { id: newVoiceModel.id } },
-          seedSound: { connect: { id: seedSoundJoin.seedSoundId } },
-          active: seedSoundJoin.active,
-        },
-      });
-    }
-
-    return {
-      redirect: {
-        destination: `/voicemodels/${newVoiceModel.id}/edit`,
-        permanent: false,
-      },
-    };
+    return await createForkedVoice(forkVoiceId, userId);
   }
 
   const voiceModel = await prisma.voiceModel.create({
