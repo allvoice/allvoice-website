20e650ad54c97e05b3a673060bc26f53c51f03b3
Evan Pierce <mrthinger@gmail.com>
2024-01-29 00:51:46 -0800

    fix signup page centering

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <SignUp />
+    </div>
+  );
 }
